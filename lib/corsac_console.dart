149b30633f40659faa70aa22dba84ccce7deeef8
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2016-03-10 19:01:04 -0800

    Removed default kernel module since it is not really necessary

--- lib/corsac_console.dart
+++ lib/corsac_console.dart
@@ -10,28 +10,23 @@ import 'package:corsac_kernel/corsac_kernel.dart';
 
 export 'package:args/command_runner.dart';
 
-class ConsoleKernelModule extends KernelModule {
-  @override
-  Map getServiceConfiguration(String environment) {
-    return {'console.commands': new List<Command>(),};
-  }
-}
-
 /// Console application built on top of Corsac [Kernel] and the `args` package.
 class Console {
+  /// The ID of kernel's container entry with the list of console commands.
+  final String commandsEntryId = 'console.commands';
   final CommandRunner commandRunner;
   final Kernel kernel;
 
   Console(this.kernel, String name, String description)
       : commandRunner = new CommandRunner(name, description) {
-    List<Command> commands = kernel.get('console.commands');
+    List<Command> commands = kernel.get(commandsEntryId);
     commands.forEach((_) {
       commandRunner.addCommand(_);
     });
   }
 
   Future run(List<String> args) {
-    return commandRunner.run(args).catchError((e) {
+    return new Future(() => commandRunner.run(args)).catchError((e) {
       print(e);
       var code = (e is UsageException) ? 64 : 255;
       exit(code);
